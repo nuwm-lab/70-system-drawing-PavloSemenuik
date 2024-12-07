f654152cc08b2a61ad4687a5eccd3538aea5c91d
PavloSemenuik <semeniuk_ak23@nuwm.edu.ua>
2024-12-07 03:05:11 +0200

    лаб7

--- Program.cs
+++ Program.cs
@@ -7,12 +7,22 @@ namespace LabWork
 {
     public class GraphForm : Form
     {
-        public GraphForm()
+        private double tMin;
+        private double tMax;
+        private double step;
+
+        public GraphForm(double tMin, double tMax, double step)
         {
+            this.tMin = tMin;
+            this.tMax = tMax;
+            this.step = step;
+
             // Налаштування форми
             this.Text = "Графік функції";
             this.Size = new Size(800, 600);
             this.Resize += (s, e) => this.Invalidate(); // Перемальовуємо при зміні розміру
+
+            AddControls(); // Додаємо елементи керування
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -20,9 +30,9 @@ namespace LabWork
             base.OnPaint(e);
 
             // Параметри графіку
-            double tMin = 2.1;
-            double tMax = 8.5;
-            double step = 0.7;
+            double tMin = this.tMin;
+            double tMax = this.tMax;
+            double step = this.step;
 
             // Отримання графічного об'єкта
             Graphics g = e.Graphics;
@@ -34,7 +44,7 @@ namespace LabWork
 
             // Межі для побудови графіку
             float margin = 50;
-            RectangleF graphBounds = new RectangleF(
+            RectangleF graphAreaBounds = new RectangleF(
                 margin,
                 margin,
                 width - 2 * margin,
@@ -42,9 +52,7 @@ namespace LabWork
             );
 
             // Малюємо осі
-            Pen axisPen = new Pen(Color.Black, 2);
-            g.DrawLine(axisPen, graphBounds.Left, graphBounds.Bottom, graphBounds.Right, graphBounds.Bottom); // X-вісь
-            g.DrawLine(axisPen, graphBounds.Left, graphBounds.Bottom, graphBounds.Left, graphBounds.Top); // Y-вісь
+            DrawAxes(g, graphAreaBounds);
 
             // Вираховуємо значення функції
             double[] tValues = GetTValues(tMin, tMax, step);
@@ -62,8 +70,8 @@ namespace LabWork
             PointF[] points = new PointF[tValues.Length];
             for (int i = 0; i < tValues.Length; i++)
             {
-                float x = (float)((tValues[i] - tMin) / (tMax - tMin) * graphBounds.Width + graphBounds.Left);
-                float y = (float)((1 - (yValues[i] - yMin) / (yMax - yMin)) * graphBounds.Height + graphBounds.Top);
+                float x = (float)((tValues[i] - tMin) / (tMax - tMin) * graphAreaBounds.Width + graphAreaBounds.Left);
+                float y = (float)((1 - (yValues[i] - yMin) / (yMax - yMin)) * graphAreaBounds.Height + graphAreaBounds.Top);
                 points[i] = new PointF(x, y);
             }
 
@@ -77,8 +85,15 @@ namespace LabWork
             // Підписи осей
             Font font = new Font("Arial", 10);
             Brush brush = Brushes.Black;
-            g.DrawString("X (t)", font, brush, graphBounds.Right - 30, graphBounds.Bottom + 10);
-            g.DrawString("Y", font, brush, graphBounds.Left - 20, graphBounds.Top - 10);
+            g.DrawString("X (t)", font, brush, graphAreaBounds.Right - 30, graphAreaBounds.Bottom + 10);
+            g.DrawString("Y", font, brush, graphAreaBounds.Left - 20, graphAreaBounds.Top - 10);
+        }
+
+        private void DrawAxes(Graphics g, RectangleF graphAreaBounds)
+        {
+            Pen axisPen = new Pen(Color.Black, 2);
+            g.DrawLine(axisPen, graphAreaBounds.Left, graphAreaBounds.Bottom, graphAreaBounds.Right, graphAreaBounds.Bottom); // X-вісь
+            g.DrawLine(axisPen, graphAreaBounds.Left, graphAreaBounds.Bottom, graphAreaBounds.Left, graphAreaBounds.Top); // Y-вісь
         }
 
         private double[] GetTValues(double tMin, double tMax, double step)
@@ -98,9 +113,42 @@ namespace LabWork
             for (int i = 0; i < tValues.Length; i++)
             {
                 double t = tValues[i];
-                yValues[i] = (t - Math.Log(2 * t)) / (3 * t + 1);
+                try
+                {
+                    yValues[i] = (t - Math.Log(2 * t)) / (3 * t + 1);
+                }
+                catch (Exception ex)
+                {
+                    yValues[i] = double.NaN; // або інше значення за замовчуванням
+                }
             }
             return yValues;
+        }
+
+        private void AddControls()
+        {
+            TextBox tMinBox = new TextBox() { Location = new Point(10, 10), Width = 100, Text = tMin.ToString() };
+            TextBox tMaxBox = new TextBox() { Location = new Point(120, 10), Width = 100, Text = tMax.ToString() };
+            TextBox stepBox = new TextBox() { Location = new Point(230, 10), Width = 100, Text = step.ToString() };
+            Button applyButton = new Button() { Text = "Apply", Location = new Point(340, 10) };
+
+            applyButton.Click += (s, e) =>
+            {
+                if (double.TryParse(tMinBox.Text, out double tMin) &&
+                    double.TryParse(tMaxBox.Text, out double tMax) &&
+                    double.TryParse(stepBox.Text, out double step))
+                {
+                    this.tMin = tMin;
+                    this.tMax = tMax;
+                    this.step = step;
+                    this.Invalidate(); // Перемалювати графік
+                }
+            };
+
+            this.Controls.Add(tMinBox);
+            this.Controls.Add(tMaxBox);
+            this.Controls.Add(stepBox);
+            this.Controls.Add(applyButton);
         }
 
         [STAThread]
@@ -108,7 +156,7 @@ namespace LabWork
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GraphForm());
+            Application.Run(new GraphForm(2.1, 8.5, 0.7));
         }
     }
 }
